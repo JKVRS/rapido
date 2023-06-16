2b483f01d72b756053de7d38f8269c99159de043
JKVRS <128927884+JKVRS@users.noreply.github.com>
2023-06-16 19:18:36 +0530

    Initial commit

---
+++ src/main/java/com/development/rapido/RapidoApplication.java
@@ -0,0 +1,13 @@
+package com.development.rapido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidoApplication.class, args);
+	}
+
+}
